609af6ae1d5cc3cf8544184f6893f0fd25a67d20
anamikasawhney <anamika.sawhney22@gmail.com>
2021-06-01 16:19:06 +0530

    Add project files.

---
+++ WebApplication4/Controllers/FirstController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult first()
+        {
+            return View();
+        }
+
+        public string second()
+        {
+            return "This is a string";
+        }
+
+        public IActionResult third()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult fourth()
+        {
+            return RedirectToAction("index", "home");
+        }
+
+        public IActionResult fifth()
+        {
+            ViewBag.data = "Batch Code is B001";
+            ViewData["name"] = "Ajay Sood";
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+namespace WebApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student() { Id = 1, Name = "Deepak", Batch = "B001", Marks = 90 };
+            ViewBag.student = student;
+            return View();
+        }
+
+        public IActionResult Display()
+        {
+            Student student = new Student() { Id = 1, Name = "Deepak", Batch = "B001", Marks = 90 };
+            
+            return View(student);
+        }
+    }
+}
